f24d8a1b854fc56e28935727d9faa4347dcdb501
jcdenton <ivan.bruzgin@gmail.com>
2015-10-04 22:11:01 +0300

    Generics

--- src/vi_generics/_28_GenericFunctions.kt
+++ src/vi_generics/_28_GenericFunctions.kt
@@ -20,12 +20,21 @@ fun task28() = TODO(
         }
 )
 
+fun <E, C : MutableCollection<E>> Collection<E>.partitionTo(matched: C, notMatched: C,
+                                                            predicate: (E) -> Boolean): Pair<C, C> {
+    forEach {
+        if (predicate(it)) matched += it
+        else notMatched += it
+    }
+    return Pair(matched, notMatched)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task28()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+//    task28()
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task28()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+//    task28()
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
